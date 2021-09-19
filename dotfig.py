75b6d7c513054636a8210018a2ea85ce5d8fd236
DevAtDawn <devatdawn@gmail.com>
2021-09-19 13:47:04 -0600

    update

--- dotfig.py
+++ dotfig.py
@@ -1,82 +1,75 @@
 from pathlib import Path
 import sys
 
-home_dir = Path.home()
-current_dir = Path.cwd()
+def run():
 
-try:
-    user_input = sys.argv[1]
-    directory_name = Path(user_input)
-    dotfiles_path = directory_name.resolve()
-except:
-    print('Please pass a directory name')
-    sys.exit(1)
+	try:
+		user_input = sys.argv[1]
+	except:
+		print('Please pass a directory name')
+		sys.exit(1)
 
+	directory_name = Path(user_input)
+	dotfiles_path = directory_name.resolve()
+	home_dir = Path.home()
+	current_dir = Path.cwd()
 
-# dotfiles_path = directory_name.resolve()
-dots_all = dotfiles_path.rglob('*')
-dots_list = [x for x in dots_all]
-dots_files = [x for x in dots_list if x.is_file()]
-dots_dirs = [x for x in dots_list if x.is_dir()]
+	def sync_dotfiles():
+		for y in dots_dirs:
+			dir_in = Path(y)
+			dir_name = dir_in.relative_to(dotfiles_path)
+			dir_out = home_dir / dir_name
+			check_dir(dir_out)
+		for x in dots_files:
+			dot_in = Path(x)
+			dot_name = dot_in.relative_to(dotfiles_path)
+			dot_out = home_dir / dot_name
+			check_file(dot_out)
+			make_links(dot_in, dot_out)
 
+	def make_links(dotin, dotout):
+		dotout.symlink_to(dotin)	
 
-def sync_dotfiles():
-	for y in dots_dirs:
-		dir_in = Path(y)
-		dir_name = dir_in.relative_to(dotfiles_path)
-		dir_out = home_dir / dir_name
-		check_dir(dir_out)
+	def check_file(thefile):
+		if thefile.is_symlink():
+			thefile.unlink()
+		else: pass
+		if thefile.is_file():
+			rename_old(thefile)
+		else: pass
 
-	for x in dots_files:
-		dot_in = Path(x)
-		dot_name = dot_in.relative_to(dotfiles_path)
-		dot_out = home_dir / dot_name
-		check_file(dot_out)
-		make_links(dot_in, dot_out)
+	def check_dir(thedir):
+		if thedir.is_dir():
+		    pass
+		else:
+		    thedir.mkdir(parents=True, exist_ok=True)
 
+	def rename_old(thefile):
+		fullname = thefile
+		fname = thefile.stem
+		fsuffix = thefile.suffix
+		fdir = thefile.parents[0]
+		new_name = Path(fname + '_old' + fsuffix)
+		path_old = fdir / new_name
+		fullname.rename(path_old)
 
-def make_links(dotin, dotout):
-	dotout.symlink_to(dotin)
-	
+	def check_exist(usrdir):
+		# if usrdir.exists():
+		if usrdir.is_dir():
+		    return True    
+		else:
+		    return False
 
-def check_file(thefile):
-	if thefile.is_symlink():
-		thefile.unlink()
-	else: pass
-	if thefile.is_file():
-		rename_old(thefile)
-	else: pass
-
-
-def check_dir(thedir):
-	if thedir.is_dir():
-	    pass
+	# def run():
+	if check_exist(dotfiles_path):
+		dots_all = dotfiles_path.rglob('*')
+		dots_list = [x for x in dots_all]
+		dots_files = [x for x in dots_list if x.is_file()]
+		dots_dirs = [x for x in dots_list if x.is_dir()]
+		sync_dotfiles()
+		print("Synced Dotfiles")
 	else:
-	    thedir.mkdir(parents=True, exist_ok=True)
+		print("invalid Directory")
 
+run()
 
-def check_exist(usrdir):
-	# if usrdir.exists():
-	if usrdir.is_dir():
-	    return True    
-	else:
-	    return False
-
-def rename_old(thefile):
-	fullname = thefile
-	fname = thefile.stem
-	fsuffix = thefile.suffix
-	fdir = thefile.parents[0]
-	new_name = Path(fname + '_old' + fsuffix)
-	path_old = fdir / new_name
-	fullname.rename(path_old)
-
-	
-if check_exist(dotfiles_path):
-	print("Syncing Dotfiles")
-	sync_dotfiles()
-else:
-	print("Invalid Directory")
-# def revert_old():
-
-# sync_dotfiles()
